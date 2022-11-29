b567b177053b2f99777a54877b79b190447c6479
Matt MacGregor <matthew.macgregor.g@gmail.com>
2022-11-28 19:39:02 -0500

    Fix Status problems

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ import mongoose from "mongoose";
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-// "mongodb+srv://mmac:<password>@cluster0.huly1u2.mongodb.net/?retryWrites=true&w=majority"
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
-
 const app = express()
 app.use(cors())
 app.use(express.json());
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -11,13 +11,13 @@ const updateTuit = async (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
   const updates = req.body;
   const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
-  res.sendStatus(status);
+  res.sendStatus((status.acknowledged && status.modifiedCount == 1 && status.matchedCount == 1) ? 200 : 403);
 }
 
 const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
   const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-  res.sendStatus(status);
+  res.sendStatus((status.acknowledged && status.deletedCount == 1) ? 200 : 403);
 }
 
 const createTuit = async (req, res) => {
