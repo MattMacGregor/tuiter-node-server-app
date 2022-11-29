1dd63463f2d0c57653e7a9516505c7f5deb04b0a
Matt MacGregor <matthew.macgregor.g@gmail.com>
2022-11-28 20:10:05 -0500

    Update schema to handle likes, retuits, images, etc

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   tuit: String,
+  image: String,
+  dislikes: Number,
+  disliked: Boolean,
   likes: Number,
   liked: Boolean,
+  retuits: Number,
+  replies: Number
 }, {collection: 'tuits'});
 export default schema;
